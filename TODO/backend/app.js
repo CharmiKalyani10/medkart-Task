e65d9a12e016fe50e39807c11038732a5b4fcab3
Himansheee <himanshi.r@medkart.in>
2024-01-16 10:38:25 +0530

    Added files

---
+++ TODO/backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+require("./conn/conn");
+const auth = require("./routes/auth");
+const list = require("./routes/list");
+app.use(cors())
+app.use(express.json());
+
+app.use("/api/v1", auth);
+app.use("/api/v2", list);
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Server is Started");
+});
+
+app.listen(1000, () => {
+  console.log("Server Started at port 1000");
+});
---
+++ TODO/backend/routes/list.js
@@ -0,0 +1,79 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const List = require("../models/list");
+
+
+// add task
+router.post("/addTask", async(req,res) => {
+  try {
+    const {title, body, email} = req.body;
+    console.log(title);
+
+    const existingUser = await User.findOne({email});
+    console.log(existingUser);
+    if(existingUser)
+    {
+        const list = new List({title,body, user:existingUser});
+        await list.save().then(() => res.status(200).json({list}));
+        existingUser.list.push(list);
+        existingUser.save();
+    }
+    
+  } catch (error) {
+    console.log(error);
+  } 
+});
+
+
+
+// update task
+router.put("/updateTask/:id", async(req,res) => {
+    try {
+      const {title, body, email} = req.body;
+      console.log(title);
+  
+      const existingUser = await User.findOne({email});
+      console.log(existingUser);
+      if(existingUser)
+      {
+         const list =  await List.findByIdAndUpdate(req.params.id, {title,body});
+          list.save().then(() => res.status(200).json({message: "Task Updated!"}));
+      }
+      
+    } catch (error) {
+      console.log(error);
+    } 
+  });
+
+
+// delete task
+router.delete("/deleteTask/:id", async(req,res) => {
+    try {
+      const {email} = req.body;
+      console.log(email);
+  
+      const existingUser = await User.findOne({email});
+      console.log(existingUser);
+      if(existingUser)
+      {
+          await List.findByIdAndDelete(req.params.id).then(() => res.status(200).json({message: "Task Deleted!"}));
+      }
+      
+    } catch (error) {
+      console.log(error);
+    } 
+  });
+
+// get the task
+router.get("/getTask/:id", async(req,res) => {
+    const list = await List.find({user: req.params.id}).sort({createdAt: -1});
+    if(list.length !==0 )
+    {
+        res.status(200).json({"message": "No Task"});
+    }
+    res.status(200).json({list});
+})
+
+
+
+module.exports = router;
